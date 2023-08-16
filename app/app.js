f70801d7c58e95f6a8fcaf7e7ef986f3208bd6d7
arohere <roshanabbas2003@gmail.com>
2023-08-17 02:47:45 +0530

    small app

---
+++ app/app.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return (
+        <View>
+            <Text>My App</Text>
+        </View>
+        );
+    }
+};
